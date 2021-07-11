db8911a725176c00ebdf10541ce49e991c784558
benwade91 <70988204+benn925@users.noreply.github.com>
2021-07-11 08:19:24 -0600

    encryption function operational

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
-main
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+# direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+text = input("Type your message:\n").lower()
+shift = int(input("Type the shift number:\n"))
+
+def split(word):
+    return [char for char in word]
+
+def encrypt():
+    word = split(text)
+    for i in range(len(text)):
+        index_to_change = alphabet.index(word[i])
+        index_to_change += shift
+        if(index_to_change > 25):
+            index_to_change -= 26
+        elif(index_to_change < 0):
+            index_to_change += 26
+        word[i] = alphabet[index_to_change]
+    print(''.join(word))
+encrypt()
