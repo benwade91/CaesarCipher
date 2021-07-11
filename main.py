45a489e87dbb786a1f192251a3533b602598175c
benwade91 <70988204+benn925@users.noreply.github.com>
2021-07-11 10:26:44 -0600

    added support for spaces in cipher

--- main.py
+++ main.py
@@ -1,24 +1,22 @@
-alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', ' ']
 
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+direction = input("Type '1' to encrypt, type '2' to decrypt:\n")
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
 def split(word):
     return [char for char in word]
 
-def encrypt():
-    global shift
-    if(direction == 'decode'):
+def encrypt(shift):
+    if(direction == 2):
         shift = 0-shift
     word = split(text)
     for i in range(len(text)):
-        index_to_change = alphabet.index(word[i])
-        index_to_change += shift
-        if(index_to_change > 25):
-            index_to_change -= 26
+        index_to_change = alphabet.index(word[i]) + shift
+        if(index_to_change > 26):
+            index_to_change -= 27
         elif(index_to_change < 0):
-            index_to_change += 26
+            index_to_change += 27
         word[i] = alphabet[index_to_change]
     print(''.join(word))
-encrypt()
+encrypt(shift)
